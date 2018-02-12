993e6c337749e76581004fe33805039cff235f51
Martin Hoppenheit <martin@hoppenheit.info>
2018-02-12 22:17:50 +0100

    Haskell: Exercise 3.

---
+++ haskell/package-xml/src/exercise-3.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Data.Char
+import Text.XML.Light
+
+main :: IO ()
+main = do
+    xml <- readFile "../../xml/example.xml"
+    putStrLn $ exercise xml
+
+exercise :: String -> String
+exercise = unlines . map (showContent . transform) . parseXML
+
+-- | Make title elements uppercase, return all other content unchanged.
+transform :: Content -> Content
+transform (Elem e)
+    | elName e == dc "title" = Elem e {elContent = map uc $ elContent e}
+    | otherwise = Elem e {elContent = map transform $ elContent e}
+  where
+    uc (Text t) = Text t {cdData = map toUpper $ cdData t}
+    uc x = x
+transform x = x
+
+-- | Create a QName in the Dublin Core namespace.
+dc :: String -> QName
+dc n = QName n ns prefix
+  where
+    ns = Just "http://purl.org/dc/elements/1.1/"
+    prefix = Just "e"
