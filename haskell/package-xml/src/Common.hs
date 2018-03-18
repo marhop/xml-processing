45f67fa3723c93ccbca41fc955bb80971b7ce100
Martin Hoppenheit <martin@hoppenheit.info>
2018-03-18 21:32:59 +0100

    Haskell: Fix namespace problem.

--- haskell/package-xml/src/Common.hs
+++ haskell/package-xml/src/Common.hs
@@ -2,8 +2,11 @@ module Common
     ( gtr
     , dc
     , findPath
+    , fixNamespace
     ) where
 
+import Data.List (nub)
+import Data.Maybe (fromMaybe, isJust)
 import Text.XML.Light
 
 -- | Create a QName in the Generic Tree namespace.
@@ -24,3 +27,23 @@ dc n = QName n ns prefix
 -- a simple XPath expression. Note that @findPath [] e = [e]@.
 findPath :: [QName] -> Element -> [Element]
 findPath ns e = foldl (\es n -> concatMap (findChildren n) es) [e] ns
+
+-- | Add namespace declarations to an element for all namespaces that are used
+-- in this element and its children. This fixes an issue with the Text.XML.Light
+-- module not adding namespace URIs to XML output, see
+-- <https://github.com/GaloisInc/xml/issues/10>.
+fixNamespace :: Content -> Content
+fixNamespace (Elem e) = Elem e {elAttribs = nub $ elAttribs e ++ namespaces e}
+fixNamespace x = x
+
+-- | Find all namespaces that are used in an element and its children and create
+-- a list of namespace declaration attributes.
+namespaces :: Element -> [Attr]
+namespaces e
+    | hasNamespace e = a : concatMap namespaces (elChildren e)
+    | otherwise = concatMap namespaces (elChildren e)
+  where
+    hasNamespace = isJust . qURI . elName
+    p = fromMaybe "" . qPrefix $ elName e
+    u = fromMaybe "" . qURI $ elName e
+    a = Attr (blank_name {qName = p, qPrefix = Just "xmlns"}) u
--- haskell/package-xml/src/Exercise4.hs
+++ haskell/package-xml/src/Exercise4.hs
@@ -2,21 +2,22 @@ module Exercise4
     ( exercise
     ) where
 
-import Data.List (nub)
-import Data.Maybe (fromMaybe)
 import Text.XML.Light
 
 import Common
 
 exercise :: String -> String
-exercise = unlines . map (showContent . transform) . parseXML
+exercise = unlines . map (showContent . fixNamespace . transform) . parseXML
 
+-- | Append the new child to the target element, return all other content
+-- unchanged.
 transform :: Content -> Content
 transform (Elem e)
     | target e = Elem e {elContent = elContent e ++ [newItem]}
     | otherwise = Elem e {elContent = map transform $ elContent e}
 transform x = x
 
+-- | Determine whether an element is the target section of this exercise.
 target :: Element -> Bool
 target e = title == "A Song of Ice and Fire"
   where
@@ -25,6 +26,7 @@ target e = title == "A Song of Ice and Fire"
             [] -> ""
             (e:_) -> strContent e
 
+-- | The new item (child element for target section).
 newItem :: Content
 newItem = Elem node
   where
@@ -43,6 +45,3 @@ newItem = Elem node
     date =
         blank_element
         {elName = dc "date", elContent = [Text (blank_cdata {cdData = "2005"})]}
-
--- TODO Problem: No namespace URI in output. See
--- <https://github.com/GaloisInc/xml/issues/10>.
